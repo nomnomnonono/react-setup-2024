02c2f3b719873a959c8d7052d8421ac1d2a22f5e
nomnomnonono <murumuru1204@gmail.com>
2024-02-29 12:19:00 +0900

    add script to postscript package.json

---
+++ postscript-package.js
@@ -0,0 +1,28 @@
+import { readFileSync, writeFileSync } from "fs";
+
+const packageJson = JSON.parse(readFileSync("package.json", "utf8"));
+
+packageJson["lint-staged"] = {
+  "*.{ts,tsx}": "eslint --fix",
+  "*": "prettier --write --ignore-unknown",
+};
+
+packageJson.scripts = {
+  dev: "vite",
+  build: "tsc && vite build",
+  preview: "vite preview",
+  test: "vitest run --coverage",
+  "update-snapshots": "vitest run --update",
+  lint: "run-p lint:*",
+  "lint:tsc": "tsc --noEmit",
+  "lint:prettier": "prettier . --check",
+  "lint:eslint": "eslint . --ext .ts,.tsx",
+  fix: "run-s fix:*",
+  "fix:prettier": "prettier . --write",
+  "fix:eslint": "eslint . --ext .ts,.tsx --fix",
+  prepare: "husky install",
+  storybook: "storybook dev -p 6006",
+  "build-storybook": "storybook build",
+};
+
+writeFileSync("package.json", JSON.stringify(packageJson, null, 2));
